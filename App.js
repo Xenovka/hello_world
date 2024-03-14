2fedc08c835a684d581f9ee42b9228a8ad43b0ad
Xenovka <wendyyyanto@gmail.com>
2024-03-14 12:41:13 +0700

    Implementing state and using Pressable component for pressing functionality

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
 
 import ImageOption from "./src/components/ImageOption";
@@ -7,13 +7,21 @@ import styles from "./App.styles";
 import question from "./assets/data/oneQuestionWithOption";
 
 const App = () => {
+    const [selected, setSelected] = useState(null);
+
     return (
         <View style={styles.root}>
             <Text style={styles.title}>{question.question}</Text>
 
             <View style={styles.optionsContainer}>
                 {question.options.map((option) => (
-                    <ImageOption key={option.id} image={option.image} text={option.text} />
+                    <ImageOption
+                        key={option.id}
+                        image={option.image}
+                        text={option.text}
+                        isSelected={selected?.id === option.id}
+                        onPress={() => setSelected(option)}
+                    />
                 ))}
             </View>
         </View>
--- src/components/ImageOption/ImageOption.js
+++ src/components/ImageOption/ImageOption.js
@@ -1,25 +1,29 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
 import PropTypes from "prop-types";
 
 import styles from "./styles";
 
-const ImageOption = ({ image, text }) => {
+const ImageOption = ({ image, text, isSelected, onPress }) => {
     return (
-        <View style={styles.optionContainer}>
+        <Pressable onPress={onPress} style={[styles.optionContainer, isSelected ? styles.selectedContainer : {}]}>
             <Image source={{ uri: image }} resizeMode="contain" style={styles.optionImage} />
-            <Text style={styles.optionText}>{text}</Text>
-        </View>
+            <Text style={isSelected ? styles.selectedText : styles.optionText}>{text}</Text>
+        </Pressable>
     );
 };
 
 ImageOption.propTypes = {
     image: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired
+    text: PropTypes.string.isRequired,
+    isSelected: PropTypes.bool,
+    onPress: PropTypes.func
 };
 
 ImageOption.defaultProps = {
-    text: "Default"
+    text: "Default",
+    isSelected: false,
+    onPress: () => {}
 };
 
 export default ImageOption;
