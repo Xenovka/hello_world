d8376a6a720722bb40ed5e65a5634e90d6710894
Xenovka <wendyyyanto@gmail.com>
2024-03-17 17:45:31 +0700

    Implementing lives system (how many attempts user can do)

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import questions from "./assets/data/allQuestions";
 const App = () => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [currentQuestion, setCurrentQuestion] = useState(questions[currentQuestionIndex]);
+    const [lives, setLives] = useState(5);
 
     useEffect(() => {
         if (currentQuestionIndex >= questions.length) {
@@ -21,17 +22,34 @@ const App = () => {
         }
     }, [currentQuestionIndex]);
 
+    const restart = () => {
+        setCurrentQuestionIndex(0);
+        setLives(5);
+    };
+
     const onCorrect = () => {
         setCurrentQuestionIndex(currentQuestionIndex + 1);
     };
 
     const onWrong = () => {
-        Alert.alert("Incorrect!");
+        if (lives <= 1) {
+            Alert.alert("Game Over!", "Try Again", [
+                {
+                    text: "Try Again",
+                    onPress: () => {
+                        restart();
+                    }
+                }
+            ]);
+        } else {
+            Alert.alert("Incorrect!");
+            setLives(lives - 1);
+        }
     };
 
     return (
         <View style={styles.root}>
-            <Header progress={currentQuestionIndex / questions.length} />
+            <Header progress={currentQuestionIndex / questions.length} lives={lives} />
 
             {currentQuestion.type === "IMAGE_MULTIPLE_CHOICE" ? (
                 <ImageMultipleChoiceQuestion question={currentQuestion} onCorrect={onCorrect} onWrong={onWrong} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,37 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+
 import ProgressBar from "../ProgressBar";
 
-const Header = ({ progress }) => {
+import heart from "../../../assets/images/heart.png";
+
+const Header = ({ progress, lives }) => {
     return (
         <View style={styles.root}>
             <ProgressBar progress={progress} />
+
+            <Image source={heart} style={styles.icon} resizeMode="contain" />
+            <Text style={styles.lives}>{lives}</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     root: {
-        flexDirection: "row"
+        flexDirection: "row",
+        alignItems: "center"
+    },
+
+    icon: {
+        height: 30,
+        width: 30,
+        marginHorizontal: 10
+    },
+
+    lives: {
+        color: "red",
+        fontWeight: "bold",
+        fontSize: 18
     }
 });
 
